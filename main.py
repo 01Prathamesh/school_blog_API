01c71216b3defffdcf0e58458f81aaeb631229dc
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-10-27 13:07:02 +0530

    Logging configuration functionality is added.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-# main.py
+import logging
 from fastapi import FastAPI
-from school_blog.routes import router # type: ignore
+from school_blog.routes import router  # type: ignore
+
+# Configure logging
+logging.basicConfig(level=logging.WARNING)  # Only show warnings and above
 
 app = FastAPI()
 
